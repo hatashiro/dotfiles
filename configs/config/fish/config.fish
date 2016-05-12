ff60045c9399c745c42654bea8b3d0c63eb7277b
Jun <me@noraesae.net>
2016-05-13 02:15:30 +0900

    Remove fish_greeting

---
+++ configs/config/fish/config.fish
@@ -0,0 +1,28 @@
+# tmux
+if status --is-interactive
+  if test -z (echo $TMUX)
+    tmux
+  end
+end
+
+# aliases
+alias vim 'nvim'
+alias pypy 'pypy3.2'
+alias tmux-reload 'tmux source-file ~/.tmux.conf'
+alias git 'hub'
+
+# settings
+set -x EDITOR nvim
+set -x TERM xterm-256color
+set -e fish_greeting
+
+# path
+set -x PATH /usr/local/bin $PATH
+set -x PATH $PATH $HOME/.dotfiles/scripts
+set -x PATH $PATH $HOME/bin
+set -x PATH $PATH $HOME/Works/env/pypy3/bin
+set -x PATH $PATH $HOME/.local/bin
+set -x PATH $PATH $HOME/.stack/programs/x86_64-osx/ghc-7.10.3/bin
+
+# fin
+fin 6
