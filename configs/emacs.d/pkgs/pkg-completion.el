fd919ca471c5d71a028aa2170ed68cc355354d6b
Hyunje Jun <me@noraesae.net>
2016-10-06 17:03:23 +0900

    Add company-mode
    It's a completion mode for Emacs.

---
+++ configs/emacs.d/pkgs/pkg-completion.el
@@ -0,0 +1,14 @@
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode)
+
+  ;; use ctrl-j/k to navigate lines
+  (define-key company-active-map (kbd "C-j") 'company-select-next)
+  (define-key company-active-map (kbd "C-k") 'company-select-previous)
+
+  ;; use C-p to start completion in insert mode
+  (define-key evil-insert-state-map (kbd "C-p") 'company-complete)
+  )
+
+(provide 'pkg-completion)
