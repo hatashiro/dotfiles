eb23a6bc0c5b5e657211d838ca33ce49fe7b37a5
Hyunje Jun <me@noraesae.net>
2016-10-06 19:09:52 +0900

    Add space highlighting

--- configs/emacs.d/custom/custom-ui.el
+++ configs/emacs.d/custom/custom-ui.el
@@ -5,4 +5,11 @@
 (global-linum-mode 1)
 (setq linum-format "%d  ")
 
+;; spaces
+(require 'whitespace)
+(global-whitespace-mode)
+(setq whitespace-style '(face trailing tabs))
+(set-face-background 'whitespace-trailing "red")
+(set-face-background 'whitespace-tab "color-236")
+
 (provide 'custom-ui)
