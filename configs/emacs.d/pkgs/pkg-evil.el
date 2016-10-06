ef563ab925aa5fd0b2ef68d21b68dadbb6da42e2
Hyunje Jun <me@noraesae.net>
2016-10-06 13:33:37 +0900

    Add some helpful settings to evil

--- configs/emacs.d/pkgs/pkg-evil.el
+++ configs/emacs.d/pkgs/pkg-evil.el
@@ -1,10 +1,21 @@
 (use-package evil
   :ensure t
 
+  :bind (
+    ;; bind ctrl-j/k to just move
+    ("C-j" . evil-next-line)
+    ("C-k" . evil-previous-line)
+    )
+
   :config
   (require 'evil)
   (evil-mode 1)
 
+  ;; command alias
+  (evil-ex-define-cmd "W" "w")
+  (evil-ex-define-cmd "Wq" "wq")
+  (evil-ex-define-cmd "WQ" "wq")
+  (evil-ex-define-cmd "Q" "q")
   )
 
 (provide 'pkg-evil)
