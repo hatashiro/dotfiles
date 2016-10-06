527190560fffbb074deaa6b033fbe07888dd14e2
Jun <me@noraesae.net>
2016-10-07 01:45:27 +0900

    Add smartparens

--- configs/emacs.d/pkgs/pkg-editor.el
+++ configs/emacs.d/pkgs/pkg-editor.el
@@ -4,4 +4,14 @@
   (add-hook 'prog-mode-hook (editorconfig-mode 1))
   (add-hook 'text-mode-hook (editorconfig-mode 1)))
 
+(use-package smartparens
+  :ensure t
+  :config
+  (require 'smartparens-config)
+  (show-smartparens-global-mode t)
+  (set-face-attribute 'sp-show-pair-match-face nil
+                      :underline nil
+                      :foreground "red")
+  )
+
 (provide 'pkg-editor)
