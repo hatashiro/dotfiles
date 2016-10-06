4b6c2afaaba51b6e5339a5d141f10bc44c995814
Hyunje Jun <me@noraesae.net>
2016-10-06 16:46:47 +0900

    Enhance UI

---
+++ configs/emacs.d/custom/custom-ui.el
@@ -0,0 +1,8 @@
+;; hide menu bar
+(menu-bar-mode 0)
+
+;; line number
+(global-linum-mode 1)
+(setq linum-format "%d  ")
+
+(provide 'custom-ui)
--- configs/emacs.d/pkgs/pkg-ui.el
+++ configs/emacs.d/pkgs/pkg-ui.el
@@ -4,7 +4,9 @@
   :init
   (custom-set-variables
     '(spacemacs-theme-custom-colors '((bg1 . "black")
-                                      (act1 . "black"))))
+                                      (act1 . "black")
+                                      (bg2 . "black")
+                                      (lnum . "brightwhite"))))
   (load-theme 'spacemacs-dark t)
   )
 
