c549d2912bbef24a25e53b7620a1326c5bd1b295
Jun <me@noraesae.net>
2016-10-08 00:34:59 +0900

    Add markdown-mode

---
+++ configs/emacs.d/pkgs/pkg-markdown.el
@@ -0,0 +1,9 @@
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . gfm-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+(provide 'pkg-markdown)
