beb1d88bc19a123502180c90b5bd63bb38f5e0d8
Jun <me@noraesae.net>
2016-10-09 20:14:42 +0900

    Fix evil-open's for haskell-mode

--- configs/emacs.d/pkgs/pkg-haskell.el
+++ configs/emacs.d/pkgs/pkg-haskell.el
@@ -1,11 +1,30 @@
+(defun haskell-evil-open-above ()
+  (interactive)
+  (evil-digit-argument-or-evil-beginning-of-line)
+  (haskell-indentation-newline-and-indent)
+  (evil-previous-line)
+  (haskell-indentation-indent-line)
+  (evil-append-line nil)
+  )
+
+(defun haskell-evil-open-below ()
+  (interactive)
+  (evil-append-line nil)
+  (haskell-indentation-newline-and-indent)
+  )
+
 (use-package intero
   :ensure t
+
   :init
   (add-hook 'haskell-mode-hook 'intero-mode)
-  (add-hook 'haskell-mode-hook
-    (lambda () (evil-leader/set-key
-                 "hi" 'intero-info
-                 "hj" 'intero-goto-definition)))
+  (add-hook 'haskell-mode-hook (lambda ()
+    (evil-leader/set-key
+      "hi" 'intero-info
+      "hj" 'intero-goto-definition)
+    ;; override open behaviours
+    (define-key evil-normal-state-map "o" 'haskell-evil-open-below)
+    (define-key evil-normal-state-map "O" 'haskell-evil-open-above)))
   )
 
 (provide 'pkg-haskell)
