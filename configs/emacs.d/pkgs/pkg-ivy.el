5d29a369cbd3fad0df1accf148bddf09c9bde801
Jun <me@noraesae.net>
2016-10-10 03:05:22 +0900

    No fuzzy

--- configs/emacs.d/pkgs/pkg-ivy.el
+++ configs/emacs.d/pkgs/pkg-ivy.el
@@ -7,10 +7,6 @@
   ;; use ctrl-j/k to navigate lines
   (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
   (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
-
-  ;; fuzzy
-  (setq ivy-re-builders-alist
-      '((t . ivy--regex-fuzzy)))
   )
 
 (provide 'pkg-ivy)
