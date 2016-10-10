eb1a7023467688c4b72d5b2aaec9086ff302efd5
Jun <me@noraesae.net>
2016-10-11 00:09:41 +0900

    Fix pbcopy in visual line
    To copy only selected part.

--- configs/emacs.d/custom/custom-copy.el
+++ configs/emacs.d/custom/custom-copy.el
@@ -1,4 +1,22 @@
-(evil-define-operator pbcopy (beg end type register)
+(evil-define-operator evil-pbcopy (beg end type register yank-handler)
+  :move-point nil
+  :repeat nil
+  (interactive "<R><x><y>")
+  (let ((evil-was-yanked-without-register
+         (and evil-was-yanked-without-register (not register))))
+    (cond
+     ((and (fboundp 'cua--global-mark-active)
+           (fboundp 'cua-copy-region-to-global-mark)
+           (cua--global-mark-active))
+      (cua-copy-region-to-global-mark beg end))
+     ((eq type 'block)
+      (shell-command-on-region beg end "pbcopy"))
+     ((eq type 'line)
+      (shell-command-on-region beg end "pbcopy"))
+     (t
+      (shell-command-on-region beg end "pbcopy")))))
+
+(evil-define-operator evil-pbcopy-line (beg end type register)
   :motion evil-line
   :move-point nil
   (interactive "<R><x>")
@@ -12,7 +30,7 @@
   (shell-command-on-region beg end "pbcopy")
   (kill-buffer "*Shell Command Output*"))
 
-(defun pbpaste ()
+(defun evil-pbpaste ()
   "Paste data from clipboard"
   (interactive)
   (evil-append-line nil)
@@ -21,8 +39,8 @@
   (shell-command-on-region (point) (if mark-active (mark) (point)) "pbpaste" nil t)
   (backward-delete-char-untabify 1))
 
-(define-key evil-motion-state-map (kbd "C-y") 'pbcopy)
-(define-key evil-normal-state-map (kbd "C-y") 'pbcopy)
-(define-key evil-normal-state-map (kbd "C-p") 'pbpaste)
+(define-key evil-motion-state-map (kbd "C-y") 'evil-pbcopy)
+(define-key evil-normal-state-map (kbd "C-y") 'evil-pbcopy-line)
+(define-key evil-normal-state-map (kbd "C-p") 'evil-pbpaste)
 
 (provide 'custom-copy)
