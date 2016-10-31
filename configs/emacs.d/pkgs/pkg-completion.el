526fb981ed5617fc9c8b53c45f95063d3029adab
Hyunje Jun <me@noraesae.net>
2016-10-31 13:48:40 +0900

    Disable downcase in company-mode

--- configs/emacs.d/pkgs/pkg-completion.el
+++ configs/emacs.d/pkgs/pkg-completion.el
@@ -3,6 +3,9 @@
   :config
   (add-hook 'after-init-hook 'global-company-mode)
 
+  ;; disable downcase
+  (setq company-dabbrev-downcase nil)
+
   ;; use ctrl-j/k to navigate lines
   (define-key company-active-map (kbd "C-j") 'company-select-next)
   (define-key company-active-map (kbd "C-k") 'company-select-previous)
