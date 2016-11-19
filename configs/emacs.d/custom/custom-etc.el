1897fc28b5033992cd0777039472260f20b6cc0c
Jun <me@noraesae.net>
2016-11-19 13:24:39 +0900

    Remove initial buffers

--- configs/emacs.d/custom/custom-etc.el
+++ configs/emacs.d/custom/custom-etc.el
@@ -1,3 +1,10 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
+
+;; removes *messages* from the buffer.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
 (provide 'custom-etc)
