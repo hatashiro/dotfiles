c6dd6b78a899b968204e4531489b2ccdc17c4060
Jun <me@noraesae.net>
2016-12-18 14:25:44 +0900

    Set JS and CSS indents in web-mode to 2

--- configs/emacs.d/pkgs/pkg-javascript.el
+++ configs/emacs.d/pkgs/pkg-javascript.el
@@ -5,7 +5,7 @@
 (use-package web-mode
   :ensure t
   :config
-  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-code-indent-offset 2)
   (setq web-mode-script-padding 0)
   (setq web-mode-markup-indent-offset 2)
   )
