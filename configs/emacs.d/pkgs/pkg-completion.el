539451be58d40e7ab435fff51956984274e6625d
Hyunje Jun <me@noraesae.net>
2017-01-31 15:14:46 +0900

    Set custom company highlight

--- configs/emacs.d/pkgs/pkg-completion.el
+++ configs/emacs.d/pkgs/pkg-completion.el
@@ -12,6 +12,12 @@
 
   ;; use C-p to start completion in insert mode
   (define-key evil-insert-state-map (kbd "C-p") 'company-complete)
+
+  ;; custom faces
+  (custom-set-faces
+   '(company-tooltip-selection ((t (:background "#262626"))))
+   '(company-scrollbar-bg ((t (:background "#303030"))))
+   '(company-scrollbar-fg ((t (:background "#3a3a3a")))))
   )
 
 (provide 'pkg-completion)
