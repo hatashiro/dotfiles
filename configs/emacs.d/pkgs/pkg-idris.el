5fee3028e3b4ca43ffc3d1d2b3465b678a8f7fd5
Hyunje Jun <me@noraesae.net>
2017-04-18 11:19:17 +0900

    Fix type faces of idris-mode

--- configs/emacs.d/pkgs/pkg-idris.el
+++ configs/emacs.d/pkgs/pkg-idris.el
@@ -1,5 +1,16 @@
 (use-package idris-mode
   :ensure t
+  :init
+  (custom-set-faces
+   '(idris-semantic-bound-face ((t (:foreground "#fe8019"))))
+   '(idris-semantic-data-face ((t (:foreground "#8ec07c"))))
+   '(idris-semantic-function-face ((t (:foreground "#d3869b"))))
+   '(idris-semantic-implicit-face ((t (:foreground "#303030"))))
+   ;; '(idris-semantic-module-face ((t (:foreground "#"))))
+   ;; '(idris-semantic-namespace-face ((t (:foreground "#"))))
+   '(idris-semantic-postulate-face ((t (:foreground "#98971a"))))
+   '(idris-semantic-type-face ((t (:foreground "#458588"))))
+   )
   )
 
 (provide 'pkg-idris)
