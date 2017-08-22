e7e87a13177f94daff0c16484a93287a2f69b673
Hyunje Jun <me@noraesae.net>
2017-08-22 12:24:34 +0900

    Disable TypeScript formatting on save

--- configs/emacs.d/pkgs/pkg-javascript.el
+++ configs/emacs.d/pkgs/pkg-javascript.el
@@ -59,7 +59,6 @@
   (setq company-tooltip-align-annotations t)
 
   ;; formats the buffer before saving
-  (add-hook 'before-save-hook 'tide-format-before-save)
   (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
   ;; format options
