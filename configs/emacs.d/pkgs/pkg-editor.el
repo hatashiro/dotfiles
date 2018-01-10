c601c1b52a08589a11e50e0e07e30768ea084cb8
Hyunje Jun <me@noraesae.net>
2018-01-10 20:04:16 +0900

    Disable auto insert concerning smartparens

--- configs/emacs.d/pkgs/pkg-editor.el
+++ configs/emacs.d/pkgs/pkg-editor.el
@@ -13,8 +13,22 @@
   :ensure t
   :config
   (require 'smartparens-config)
-  (smartparens-global-mode 1)
+  (smartparens-global-mode t)
   (show-smartparens-global-mode t)
+
+  (setq sp-autoinsert-pair nil)
+  (setq sp-autodelete-pair nil)
+  (setq sp-autodelete-closing-pair nil)
+  (setq sp-autodelete-opening-pair nil)
+  (customize-set-variable 'sp-autoskip-closing-pair nil)
+  (customize-set-variable 'sp-autoskip-opening-pair nil)
+  (setq sp-cancel-autoskip-on-backward-movement nil)
+  (setq sp-autodelete-wrap nil)
+  (setq sp-autowrap-region nil)
+  (setq sp-autoinsert-quote-if-followed-by-closing-pair nil)
+  (setq sp-escape-wrapped-region nil)
+  (setq sp-escape-quotes-after-insert nil)
+
   (set-face-attribute 'sp-show-pair-match-face nil
                       :underline nil
                       :foreground "red")
