166ed3f176bc3511e1668fc55e96922f00da006f
Hyunjae Jun <noraesae+dev@gmail.com>
2020-05-16 02:33:01 +0900

    Remove unused files.

--- scripts/remove-gems.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-`gem list --no-versions`.split("\n").each do |gem|
-  `gem list -d #{gem}`.gsub(/Installed at(.*):.*/).each do |dir|
-    dir = dir.gsub(/Installed at(.*): /,'').gsub("\n", '')
-    system "gem uninstall #{gem} -aIx -i #{dir}"
-  end
-end
